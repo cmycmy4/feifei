e7d32b901a228063a01b62e77f1b0611eb716109
cmycmy4 <624615917@qq.com>
2025-09-18 21:28:29 +0800

    add do get  index.py

--- api/index.py
+++ api/index.py
@@ -62,6 +62,12 @@ def handle_event(body):
     return {"code": 0, "msg": "success"}
 
 class handler(BaseHTTPRequestHandler):
+        def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({"code": 0, "msg": "Service is running"}).encode('utf-8'))
+    
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
